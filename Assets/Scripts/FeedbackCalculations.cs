937dec12003b8f87796b1ab15ad7eeebcebf70fd
cbb1320 <cbb1320@g.rit.edu>
2020-02-01 18:34:56 -0500

    Added Sponsors and Balance Functions to FeedbackCalcs

--- Assets/Scripts/FeedbackCalculations.cs
+++ Assets/Scripts/FeedbackCalculations.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class FeedbackCalculations : MonoBehaviour
 {
     public Character[] characters;
-    
+
+    public int sponsorChar;
+    public int sponsorProp;
+    public int sponsorValue;
+
     public int CalculateHype()
     {
         int patchHypeScore = 0;
@@ -27,7 +31,6 @@ public class FeedbackCalculations : MonoBehaviour
 
         for(int i = 0; i < 5; i++)
         {
-
             if ((characters[i].getAttackPower() + characters[i].getRange() + characters[i].getHealth()) >= 10)
             {
                 patchFunScore++;
@@ -83,18 +86,67 @@ public class FeedbackCalculations : MonoBehaviour
             return false;
         }
     }
-
+    
     public bool CheckSponsors()
     {
-        return true;
+        if(sponsorProp == 0)
+        {
+            if(characters[sponsorChar].getAttackPower() == sponsorValue)
+            {
+                return true;
+            }
+        }
+        else if (sponsorProp == 1)
+        {
+            if (characters[sponsorChar].getAttackSpeed() == sponsorValue)
+            {
+                return true;
+            }
+        }
+        else if (sponsorProp == 2)
+        {
+            if (characters[sponsorChar].getHealth() == sponsorValue)
+            {
+                return true;
+            }
+        }
+        else if (sponsorProp == 3)
+        {
+            if (characters[sponsorChar].getMobility() == sponsorValue)
+            {
+                return true;
+            }
+        }
+        else if (sponsorProp == 4)
+        {
+            if (characters[sponsorChar].getRange() == sponsorValue)
+            {
+                return true;
+            }
+        }
+        else if (sponsorProp == 5)
+        {
+            if (characters[sponsorChar].getLuck() == sponsorValue)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public bool CheckBalance()
+    public int CheckBalance()
     {
+        int patchBalanceScore = 0;
+
         for (int i = 0; i < 5; i++)
         {
+            int char1Score = 0;
+
             for (int j = i+1; j < 5; j++)
             {
+                int char2Score = 0;
+
                 // Do not compare matchup if character is the same.
                 if (i == j)
                 {
@@ -102,10 +154,86 @@ public class FeedbackCalculations : MonoBehaviour
                 }
                 // Get stats for each character
                 // Compare Stats; if one is greater, add point.
-                if()
+
+                /*Attack Power*/
+                if(characters[i].getAttackPower() > characters[j].getAttackPower())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if(characters[i].getAttackPower() < characters[j].getAttackPower())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                /*Attack Speed*/
+                if (characters[i].getAttackSpeed() > characters[j].getAttackSpeed())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if (characters[i].getAttackSpeed() < characters[j].getAttackSpeed())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                /*Health*/
+                if (characters[i].getHealth() > characters[j].getHealth())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if (characters[i].getHealth() < characters[j].getHealth())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                /*Mobility*/
+                if (characters[i].getMobility() > characters[j].getMobility())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if (characters[i].getMobility() < characters[j].getMobility())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                /*Range*/
+                if (characters[i].getRange() > characters[j].getRange())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if (characters[i].getRange() < characters[j].getRange())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                /*Luck*/
+                if (characters[i].getLuck() > characters[j].getLuck())
+                {
+                    char1Score++;
+                    char2Score--;
+                }
+                else if (characters[i].getLuck() < characters[j].getLuck())
+                {
+                    char1Score--;
+                    char2Score++;
+                }
+
+                if((char1Score - char2Score) < 1 || (char1Score - char2Score) > -1)
+                {
+                    patchBalanceScore++;
+                }
             }
         }
-        //NOTE: Create separate compare character function to test matchups that is called in checkbalance.
-        return true;
+
+        return patchBalanceScore;
     }
 }
