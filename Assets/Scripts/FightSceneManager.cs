c648c45e6158af381a2a4dfed14f3e8a5938a81e
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2020-02-02 13:57:17 -0500

    Making Fight Scene

---
+++ Assets/Scripts/FightSceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FightSceneManager : MonoBehaviour {
+
+    void Awake(){
+        
+    }
+
+    void fightTime(){
+        StartCoroutine(FightTimeBeforeResultScreen());
+    }
+
+    IEnumerator FightTimeBeforeResultScreen(){
+        yield return new WaitForSeconds(5f);
+        // FightSceneManager.LoadScene("");
+    }
+}
